a1b315e71d7c2213b150cae5339465e425190d06
Thomas <44934810+YehyunLee@users.noreply.github.com>
2026-06-26 14:23:36 -0400

    Add 10_regular_expression_matching.py - LeetCode Problem 10

---
+++ 10_regular_expression_matching.py
@@ -0,0 +1,38 @@
+class Solution:
+    # def match(text, pattern):
+    #     if not pattern:
+    #         return not text
+    #     first_match = bool(text) and pattern[0] in {text[0], "."}
+    #     return first_match and match(text[1:], pattern[1:])
+    
+    # def isMatch(self, text: str, pattern: str) -> bool:
+    #     if not pattern:
+    #         return not text
+
+    #     first_match = bool(text) and pattern[0] in {text[0], "."}
+
+    #     if len(pattern) >= 2 and pattern[1] == "*":
+    #         return (
+    #             self.isMatch(text, pattern[2:])
+    #             or first_match
+    #             and self.isMatch(text[1:], pattern)
+    #         )
+    #     else:
+    #         return first_match and self.isMatch(text[1:], pattern[1:])
+
+     def isMatch(self, s: str, p: str) -> bool:
+        @lru_cache(None)
+        def dp(i, j):
+            if j == len(p):  # If `j` reach end of pattern `p`
+                return i == len(s)  # Then it fully matches if and only if `i` reach end of string `s`
+				
+            if j+1 < len(p) and p[j+1] == '*':
+                ans = dp(i, j+2) # match zero chars
+                if i < len(s) and (s[i] == p[j] or p[j] == '.'):
+                    ans = ans or dp(i+1, j) # match 1 char, skip 1 char in s, don't skip in p since it can march one or match more characters
+                return ans
+            if p[j] == '.' or i < len(s) and s[i] == p[j]:
+                return dp(i+1, j+1) # match 1 char, skip 1 char in both s and p
+            return False
+        
+        return dp(0, 0)
